209d34019f053daac3c6994b9e6f3575d1351974
Bokyung <50052797+testingbyseo@users.noreply.github.com>
2022-04-21 14:56:05 +0900

    [Silver III] Title: 파일 정리, Time: 816 ms, Memory: 65108 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -7,38 +7,36 @@ import java.util.*;
 
 public class Main {
 
-
     static int n;
     static int answer;
-    static HashMap<String, Integer> hm;
+    
+    static Map<String, Integer> treeMap;
     static StringBuilder sb;
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         sb = new StringBuilder();
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        hm = new HashMap<>();
+        n = Integer.parseInt(st.nextToken());        
+        treeMap = new TreeMap<>();
 
         for(int i=0; i<n; i++){
             String file = br.readLine();
             String[] str = file.split("\\.");
-
-            if(str.length > 0){
-                hm.put(str[1], hm.getOrDefault(str[1], 0) + 1);
+            if(str.length > 0){                
+                treeMap.put(str[1], treeMap.getOrDefault(str[1], 0) + 1);
             }
         }
-        List<Map.Entry<String, Integer>> entries = new LinkedList<>(hm.entrySet());
 
-        Collections.sort(entries, Comparator.comparing(Map.Entry::getKey));
 
-        for(Map.Entry<String, Integer> e : entries){
+
+        // treeMap
+        for(Map.Entry<String, Integer> e : treeMap.entrySet()){
             sb.append(e.getKey()).append(" ").append(e.getValue());
             sb.append("\n");
         }
-
         bw.write(sb.toString());
         bw.flush();
         bw.close();
